0137bc8ed58bdf5f569af6649bb0d07803a73e73
ErickMacena <erickmacena2@gmail.com>
2021-08-26 11:58:48 -0300

    Form and List Items separated in Components

--- src/Todo.js
+++ src/Todo.js
@@ -1,36 +1,23 @@
 import React, { useState } from 'react'
+import List from './List';
+import TodoForm from './TodoForm';
 import './Todo.css'
 
 function Todo() {
 
-    const [text, setText] = useState("");
     const [items, setItems] = useState([]);
 
-    function handleChange(event) {
-        let t = event.target.value;
-        setText(t);
-    }
-
-    function addItem(event) {
-        event.preventDefault();
-
-        if(!text) return;
-
-        setItems([...items, text]);
-        setText("");
+    function addItem(item) {
+        setItems([...items, item])
     }
 
     return (
         <div className="container">
             <h1>Todo</h1>
-            <form>
-                <input onChange={handleChange} type="text" value={text}/>
-                <button onClick={addItem}>Add</button>
-            </form>
 
-            <ul>
-                {items.map(item => <li>{item}</li>)}
-            </ul>
+            <TodoForm onAddItem={addItem}></TodoForm>
+
+            <List items={items}></List>
         </div>
     )
 }
